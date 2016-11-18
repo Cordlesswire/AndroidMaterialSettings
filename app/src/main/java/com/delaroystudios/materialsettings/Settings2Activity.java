250f27c564cdec0ba21a3112cb8a7a513a0fd367
Delaroy Studios <bamoyk@yahoo.com>
2016-11-18 03:58:25 -0800

    first commit

---
+++ app/src/main/java/com/delaroystudios/materialsettings/Settings2Activity.java
@@ -0,0 +1,5 @@
+package com.delaroystudios.materialsettings;
+
+public class Settings2Activity extends Settings1Activity {
+
+}
---
+++ app/src/main/java/com/delaroystudios/materialsettings/Settings3Activity.java
@@ -0,0 +1,5 @@
+package com.delaroystudios.materialsettings;
+
+public class Settings3Activity extends Settings1Activity {
+
+}
